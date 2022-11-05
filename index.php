29b8fb4278d6aad4cc109d18e128be1ad9a8d3c0
Juan Senen <juansenen@hotmail.com>
2022-11-05 09:28:42 +0100

    Cambios de prueba en index

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
- include "./includes/header.php"; ?>
+    include "./includes/header.php";
+    include "./domain/Cliente.php";
 
+    $cliente = new Cliente();
+    $cliente->nombre = "Juan Senen";
+    $cliente->dni = "1234567A";
+    $cliente->numeroCuenta = "A123";
+?>
+<p>Cliente <?php var_dump($cliente); ?></p>
 </body>
 </html>
